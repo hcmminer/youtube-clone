46b1cc993ded6b2cae455e5c1dd991c9d99f2b20
phuong ban <phuongban@MacBook-Air-cua-phuong.local>
2021-12-28 23:53:13 +0700

    ok

--- seeder.js
+++ seeder.js
@@ -15,9 +15,39 @@ mongoose.connect(process.env.MONGO_URI, {}).then(
 );
 
 const categories = JSON.parse(
-  fs.readFileSync(`${__dirname / _data / categories.json}`, utf - 8)
+  fs.readFileSync(`${__dirname}/_data/categories.json`, "utf-8")
 );
 
+const importData = async () => {
+  try {
+    await Category.create(categories);
+    console.log("Data Imported".green.inverse);
+    // neu thanh cong thi ket thuc terminal
+    process.exit(); // tham so mac dinh la 0
+  } catch (err) {
+    console.log(error.err);
+  }
+};
 
+const deleteData = async () => {
+  try {
+    await Category.deleteMany();
+    console.log("Data destroyed".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err.error);
+  }
+};
 
-
+switch (process.argv[2]) {
+  case "-id":
+    importData();
+    break;
+  case "-dd":
+    deleteData();
+    break;
+  default:
+    console.log("\n-id\t Import Data");
+    console.log("-dd\t Delete Data");
+    process.exit();
+}
